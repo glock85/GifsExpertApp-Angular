21098c96b1ab09dc41d8a14c231412ec22047277
Joseph Lopez <joseph@MBP-de-Joseph.lan>
2022-09-15 09:45:15 -0400

    validations search and apikey

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,13 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _historial: string[] = [];
+  private apiKey: string = 'c7uen6ccxk2BGn0aX0MQp6UUP6APbp74';
 
   get historial() {
     return [...this._historial];
   }
 
   buscarGifs(query: string) {
-    this._historial.unshift(query);
+
+    query = query.trim().toLocaleLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+
     console.log(this._historial);
   }
 
